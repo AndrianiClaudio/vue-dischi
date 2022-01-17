5bc4331617b290bc3e17b726bce0c16b64de1ffc
AndrianiClaudio <claudioandriani1994@gmail.com>
2022-01-17 15:37:13 +0100

    Creazione repo. file principali e Header.

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <Header />
+    <Main />
+  </div>
+</template>
+
+<script>
+import Header from './components/Header.vue';
+import Main from './components/Main.vue';
+
+export default {
+  name: 'App',
+  components: {
+    Header,
+    Main,
+  },
+};
+</script>
+
+<style lang="scss">
+@import './assets/scss/style.scss';
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,31 @@
+<template>
+  <header class="header">
+    <div class="container">
+      <img src="../assets/img/logo.svg" alt="Logo" class="logo" />
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+};
+</script>
+
+<style lang='scss' scoped>
+@import '../assets/scss/partials/_variables.scss';
+.header {
+  background-color: $headerBgColor;
+  .container {
+    display: flex;
+    height: 80px;
+    align-items: center;
+    padding-left: 1rem;
+      .logo {
+        height: 50px;
+        background-color: black;
+        border-radius: 50%;
+      }
+  }
+}
+</style>
